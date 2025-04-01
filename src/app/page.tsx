5c7b699ce68bf8e25ce2019e2c1a3c8f5038ec4b
Shalini2131 <shalukallepalli21@gmail.com>
2025-03-31 20:41:15 -0500

    modified home

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -61,18 +61,20 @@ export default function Home() {
           }),
         }}
       />
-      <Column fillWidth paddingY="l" gap="m">
-        <Column maxWidth="s">
-          <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
-            <Heading wrap="balance" variant="display-strong-l">
-              {home.headline}
-            </Heading>
-          </RevealFx>
-          <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
-            <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
-              {home.subline}
-            </Text>
-          </RevealFx>
+    <Column maxWidth="xl" gap="xl" horizontal="center" paddingX="xl">
+  <Column fillWidth paddingY="l" gap="m">
+    <Column maxWidth="l" paddingX="l">
+      <RevealFx translateY="4" fillWidth horizontal="start" paddingBottom="m">
+        <Heading wrap="balance" variant="display-strong-l">
+          {home.headline}
+        </Heading>
+      </RevealFx>
+      <RevealFx translateY="8" delay={0.2} fillWidth horizontal="start" paddingBottom="m">
+        <Text wrap="balance" onBackground="neutral-weak" variant="heading-default-xl">
+          {home.subline}
+        </Text>
+      </RevealFx>
+
           <RevealFx translateY="12" delay={0.4} horizontal="start">
             <Button
               id="about"
